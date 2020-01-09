b02df7206124dc18dce3d1a7f1c084973c3bb33c
Anil Nair <anilcoll90@gmail.com>
2020-01-09 17:50:03 +0530

    Changed Folder Structure and Added two labels

--- app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
+++ app/src/main/java/com/raywenderlich/timefighter/MainActivity.kt
@@ -3,6 +3,15 @@ package com.raywenderlich.timefighter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+//TODO: Add a "Tap Me" Button
+//TODO: Add Text for game score
+//TODO: Add Text for Time Left
+//TODO: When you tap the button the game starts and timer counts down
+//TODO: Tapping the button again increments the score.
+//TODO: When timer reaches zero show the score and reset the game.
+//TODO: When phone is held in landscape mode rotate the game.
+//TODO: It Should look nice.
+//TODO: Submit it to Google Play Store.
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
